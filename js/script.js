456374bc2c4def2de27245ff11dfff234aa2787a
Jesus Bohorquez <bohorquez866@gmail.com>
2020-09-11 17:01:49 -0500

    adds mobile navbar

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const burguerMenu = document.querySelector('.burger-menu');
+const navbarList = document.querySelector('.navbar__list');
+
+//open and closes mobile menu
+burguerMenu.addEventListener('click', e => {
+    navbarList.classList.toggle('show-sidebar');
+});
+
+//change color on scroll
+
+const navbar = document.querySelector('.navbar');
+const sectionOne = document.querySelector('.header');
+
+const sectionOneOpt = {
+    rootMargin: "-50px"
+};
+
+const sectionObserver = new IntersectionObserver((entries, sectionObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            navbar.classList.add('navbar--scrolled');
+        } else {
+            navbar.classList.remove('navbar--scrolled')
+        }
+    })
+
+}, sectionOneOpt)
+
+sectionObserver.observe(sectionOne);
